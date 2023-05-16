b356207f38ea264893db19582ca619175a2fe074
rowild <robertwildling@gmail.com>
2023-05-16 16:32:21 +0200

    Finished tutorial

--- effect.js
+++ effect.js
@@ -1,10 +1,31 @@
-import { getLocationsWithColor } from './utils'
+import { getLocationsWithColor, average } from './utils'
 
 export default class Effect {
     constructor(canvas, video) {
         this.canvas = canvas
         this.video = video
         this.ctx = canvas.getContext("2d", { willReadFrequently: true })
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API
+        // Theremin Sinus Sound
+        const audioCtx = new AudioContext()
+
+        this.osc = audioCtx.createOscillator()
+        this.osc.frequency.value = 0
+        this.osc.start()
+
+        this.gainNode = audioCtx.createGain()
+        this.gainNode.gain.value = 0.14
+        // this.gain.connect(audioCtx.destination)
+
+        this.osc.connect(this.gainNode).connect(audioCtx.destination)
+
+        // Volume
+        // const volumeControl = document.querySelector("#volume");
+        // volumeControl.addEventListener("input",
+        //     () => { gainNode.gain.value = volumeControl.value; },
+        //     false
+        //   );
 
         // initiate animation
         this.#animate()
@@ -17,13 +38,42 @@ export default class Effect {
         // get image (=video) data
         const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
 
-        const theLocs = getLocationsWithColor(imgData, { r:0, g:0, b:255 })
+        const locs = getLocationsWithColor(imgData, { r:0, g:0, b:255 })
 
-        // ctx.fillstyle = "yellow"
+        // This is kind of a "debugging code"
+        ctx.fillStyle = "yellow"
 
-        // locs.forEach(loc => {
-        //     ctx.fillRect(loc.x, loc.y, 1, 1)
-        // })
+        locs.forEach(loc => {
+            ctx.fillRect(loc.x, loc.y, 1, 1)
+        })
+
+        // draw a circle around the found locations
+        if(locs.length > 0) {
+            const center = average(locs)
+
+            // For setting the frequency
+            const p = 1 - center.y / canvas.height
+            const freq = 200 + 500*p // between 200 and 700; linear interpolation
+            this.osc.frequency.value = freq
+
+            // This red dot in the center of the yellow area is also kind of a debugging code
+            ctx.beginPath()
+            ctx.fillStyle = "red"
+            ctx.arc(center.x, center.y, 5, 0, Math.PI*2)
+            ctx.fill()
+
+            // Y-axis: draw a horizontal line where the marker is
+            ctx.beginPath()
+            ctx.strokeStyle = "maroon" // blue
+            ctx.lineWidth = 5
+            ctx.moveTo(0, center.y)
+            ctx.lineTo(canvas.width, center.y)
+            ctx.stroke()
+        }
+        else {
+            // set frequency to an inaudible value, if no values are found
+            this.osc.frequency.value = 0
+        }
 
         requestAnimationFrame(this.#animate.bind(this))
     }
--- utils.js
+++ utils.js
@@ -1,6 +1,10 @@
-// Distance between 2 (color) points
-const sqDistance = (p1, p2) => {
-    return (p1.r - p2.r)**2 + (p1.g - p2.g)**2 + (p1.b - p2.b)**2
+// Distance between 2 (color) points in Eukledian Space
+const distance = (c1, c2) => {
+    return Math.sqrt(sqDistance(c1,c2))
+}
+
+const sqDistance = (c1, c2) => {
+    return (c1.r - c2.r)**2 + (c1.g - c2.g)**2 + (c1.b - c2.b)**2
 }
 
 const colorMatch = (c1, c2, threshold = 160) => {
@@ -11,33 +15,45 @@ const getLocationsWithColor = (imgData, color) => {
     const locs = []
 
     // imgData.data is an array in group of fours for RGBA
-    for(let i = 0; imgData.data.length; i+=4) {
-        // const pColor = {
-        //     r:imgData.data[i],
-        //     g:imgData.data[i+1],
-        //     b:imgData.data[i+2]
-        // }
-
-        // console.log('pColor =', pColor);
+    for(let i = 0; i < imgData.data.length; i+=4) {
+        const pColor = {
+            r:imgData.data[i],
+            g:imgData.data[i+1],
+            b:imgData.data[i+2]
+        }
 
         // get x,y location
-        // const pIndex = i/4
+        const pIndex = i/4
 
-        // console.log('pIndex =', pIndex);
+        const loc = {
+            x:pIndex % imgData.width,
+            y:Math.floor(pIndex/imgData.width)
+        }
 
-        // const loc = {
-        //     x:pIndex % imgData.width,
-        //     y:Math.floor(pIndex/imgData.width)
-        // }
-
-        // console.log('loc =', loc);
-
-        // if(colorMatch(pColor, color)) {
-        //     locs.push(loc)
-        // }
+        if(colorMatch(pColor, color)) {
+            locs.push(loc)
+        }
     }
     
     return locs
 }
 
-export { getLocationsWithColor }
+// Find the average of an array (locs: Array)
+const average = (locs) => {
+    // initialise
+    const res = { x:0, y: 0 }
+
+    // summ up all points
+    locs.forEach(loc => {
+        res.x += loc.x
+        res.y += loc.y
+    })
+
+    // get the average by dividing through loc.length
+    res.x /= locs.length
+    res.y /= locs.length
+
+    return res
+}
+
+export { getLocationsWithColor, average }
